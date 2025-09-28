499ccb71cd3e024f0e8ca658fa4ea40ceaa0e6ec
lonitret <lonitret@gmail.com>
2025-09-28 17:16:26 +0700

    создание model

---
+++ Model/Model.cs
@@ -0,0 +1,22 @@
+﻿namespace Model
+{
+    public class Labubu
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Rarity { get; set; }
+        public int Size { get; set; }
+
+
+        public Labubu(int  id, string name, string color, int rarity, int size)
+        {
+            ID = id;
+            Name = name;
+            Color = color;
+            Rarity = rarity;
+            Size = size;
+        }
+    }
+
+}
