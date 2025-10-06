4f42da2b06c8ee469817b82f18df921216e3c04a
whatreallifeis <sinkina517@gmail.com>
2025-10-07 01:54:19 +0700

    Update UpdateLabubuForm.Designer.cs
    Добавлена кнопка "Новая цена" для обновления информации о цене

--- WinFormsApp/UpdateLabubuForm.Designer.cs
+++ WinFormsApp/UpdateLabubuForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp
+namespace WinFormsApp
 {
     partial class UpdateLabubuForm
     {
@@ -39,15 +39,17 @@
             cmbSize = new ComboBox();
             cmbRarity = new ComboBox();
             button1 = new Button();
+            textPrice1 = new TextBox();
+            label7 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15F);
-            label1.Location = new Point(103, 80);
+            label1.Location = new Point(107, 105);
             label1.Name = "label1";
-            label1.Size = new Size(186, 28);
+            label1.Size = new Size(230, 35);
             label1.TabIndex = 0;
             label1.Text = "Изменение лабубу";
             // 
@@ -55,9 +57,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F);
-            label2.Location = new Point(53, 154);
+            label2.Location = new Point(52, 173);
             label2.Name = "label2";
-            label2.Size = new Size(87, 21);
+            label2.Size = new Size(112, 28);
             label2.TabIndex = 1;
             label2.Text = "Новое имя";
             // 
@@ -65,9 +67,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F);
-            label3.Location = new Point(53, 183);
+            label3.Location = new Point(52, 213);
             label3.Name = "label3";
-            label3.Size = new Size(95, 21);
+            label3.Size = new Size(121, 28);
             label3.TabIndex = 2;
             label3.Text = "Новый цвет";
             // 
@@ -75,9 +77,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F);
-            label4.Location = new Point(53, 241);
+            label4.Location = new Point(52, 284);
             label4.Name = "label4";
-            label4.Size = new Size(114, 21);
+            label4.Size = new Size(147, 28);
             label4.TabIndex = 3;
             label4.Text = "Новый размер";
             // 
@@ -85,9 +87,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F);
-            label5.Location = new Point(53, 212);
+            label5.Location = new Point(52, 248);
             label5.Name = "label5";
-            label5.Size = new Size(123, 21);
+            label5.Size = new Size(156, 28);
             label5.TabIndex = 4;
             label5.Text = "Новая редкость";
             // 
@@ -95,55 +97,80 @@
             // 
             label6.Location = new Point(0, 0);
             label6.Name = "label6";
-            label6.Size = new Size(100, 23);
+            label6.Size = new Size(114, 31);
             label6.TabIndex = 0;
             // 
             // textName
             // 
-            textName.Location = new Point(182, 152);
+            textName.Location = new Point(208, 177);
+            textName.Margin = new Padding(3, 4, 3, 4);
             textName.Name = "textName";
-            textName.Size = new Size(100, 23);
+            textName.Size = new Size(114, 27);
             textName.TabIndex = 5;
             // 
             // textColor
             // 
-            textColor.Location = new Point(182, 181);
+            textColor.Location = new Point(208, 212);
+            textColor.Margin = new Padding(3, 4, 3, 4);
             textColor.Name = "textColor";
-            textColor.Size = new Size(100, 23);
+            textColor.Size = new Size(114, 27);
             textColor.TabIndex = 6;
             // 
             // cmbSize
             // 
             cmbSize.FormattingEnabled = true;
-            cmbSize.Location = new Point(182, 239);
+            cmbSize.Location = new Point(208, 284);
+            cmbSize.Margin = new Padding(3, 4, 3, 4);
             cmbSize.Name = "cmbSize";
-            cmbSize.Size = new Size(100, 23);
+            cmbSize.Size = new Size(114, 28);
             cmbSize.TabIndex = 7;
             // 
             // cmbRarity
             // 
             cmbRarity.FormattingEnabled = true;
-            cmbRarity.Location = new Point(182, 210);
+            cmbRarity.Location = new Point(208, 247);
+            cmbRarity.Margin = new Padding(3, 4, 3, 4);
             cmbRarity.Name = "cmbRarity";
-            cmbRarity.Size = new Size(100, 23);
+            cmbRarity.Size = new Size(114, 28);
             cmbRarity.TabIndex = 8;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 12F);
-            button1.Location = new Point(182, 303);
+            button1.Location = new Point(162, 402);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(100, 30);
+            button1.Size = new Size(114, 40);
             button1.TabIndex = 9;
             button1.Text = "Изменить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += btnUpdate_Click;
             // 
+            // textPrice1
+            // 
+            textPrice1.Location = new Point(208, 319);
+            textPrice1.Name = "textPrice1";
+            textPrice1.Size = new Size(114, 27);
+            textPrice1.TabIndex = 10;
+            textPrice1.TextChanged += textPrice1_TextChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F);
+            label7.Location = new Point(55, 319);
+            label7.Name = "label7";
+            label7.Size = new Size(118, 28);
+            label7.TabIndex = 11;
+            label7.Text = "Новая цена";
+            // 
             // UpdateLabubuForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(398, 375);
+            ClientSize = new Size(455, 500);
+            Controls.Add(label7);
+            Controls.Add(textPrice1);
             Controls.Add(button1);
             Controls.Add(cmbRarity);
             Controls.Add(cmbSize);
@@ -155,6 +182,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "UpdateLabubuForm";
             Text = "Изменение лабубу";
             ResumeLayout(false);
@@ -174,5 +202,7 @@
         private ComboBox cmbSize;
         private ComboBox cmbRarity;
         private Button button1;
+        private TextBox textPrice1;
+        private Label label7;
     }
 }
