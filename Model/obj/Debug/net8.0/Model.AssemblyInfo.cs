2a9c3802196d1db41cb2fcf4bee6b004d0838911
whatreallifeis <sinkina517@gmail.com>
2025-11-10 12:52:17 +0700

    debug

--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a2b2acba4a85e947e4777a45d8f39831e11156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516504d630d4414b0a32a60794aeab10513fe32d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
