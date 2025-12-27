a093416689538cb2fcd0374bf852bbe11e71565c
whatreallifeis <sinkina517@gmail.com>
2025-12-27 20:01:42 +0700

    MVVM ver. 1.0

--- WinFormsApp/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
+++ WinFormsApp/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599cb53f5e0780bbeec8342910a0b65ef8da9f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81b0c4c31bbe607de9146fb65b11433870778b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
